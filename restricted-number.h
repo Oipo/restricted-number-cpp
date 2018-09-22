0ffca390a1682a9c65cd3646fe635c378db4253c
Michael de Lang <kingoipo@gmail.com>
2018-09-22 23:01:04 +0200

    Initial commit

---
+++ restricted-number.h
@@ -0,0 +1,121 @@
+#pragma once
+
+#include <cassert>
+#include <algorithm>
+#include <type_traits>
+
+template <class T>
+struct restricted_number {
+    T minimum, maximum, current;
+
+    restricted_number(T minimum, T maximum, T current) {
+        assert(minimum < maximum);
+
+        this->minimum = minimum;
+        this->maximum = maximum;
+        set(current);
+    }
+
+
+    restricted_number(T minimum, T maximum) {
+        assert(minimum < maximum);
+
+        this->minimum = minimum;
+        this->maximum = maximum;
+        this->current = maximum;
+    }
+
+    restricted_number* set(T num) {
+        num = std::min(num, maximum);
+        num = std::max(num, minimum);
+        current = num;
+        return this;
+    }
+
+    restricted_number* add(T num) {
+        return set(current + num);
+    }
+
+    restricted_number* sub(T num) {
+        return add(-num);
+    }
+
+    restricted_number* add_over_maximum(T num) {
+        maximum += num;
+        return add(num);
+    }
+
+    restricted_number* sub_under_minimum(T num) {
+        minimum -= num;
+        return sub(num);
+    }
+
+    restricted_number* to_maximum() {
+        return set(maximum);
+    }
+
+    restricted_number* to_minimum() {
+        return set(minimum);
+    }
+
+    bool at_maximum() {
+        return current == maximum;
+    }
+
+    bool at_minimum() {
+        return current == minimum;
+    }
+
+    bool gt(T num) {
+        return current > num;
+    }
+
+    bool gte(T num) {
+        return current >= num;
+    }
+
+    bool lt(T num) {
+        return current < num;
+    }
+
+    bool lte(T num) {
+        return current <= num;
+    }
+
+    restricted_number* set_to_percent(T perc) {
+        return set(perc * ((this->maximum - this->minimum) / 100));
+    }
+
+    restricted_number* add_percent(T perc) {
+        return add(perc * ((this->maximum - this->minimum) / 100));
+    }
+
+    restricted_number* sub_percent(T perc) {
+        return add_percent(-perc);
+    }
+
+    T as_percent() {
+        if(std::numeric_limits<T>::is_integer) {
+            return std::floor((static_cast<float>(current) / static_cast<float>(maximum)) * 100.f);
+        }
+        return std::floor((current / maximum) * static_cast<T>(100));
+    }
+
+    bool gt_percent(T perc) {
+        auto p = as_percent();
+        return as_percent() > perc;
+    }
+
+    bool gte_percent(T perc) {
+        return as_percent() >= perc;
+    }
+
+    bool lt_percent(T perc) {
+        return as_percent() < perc;
+    }
+
+    bool lte_percent(T perc) {
+        return as_percent() <= perc;
+    }
+
+};
---
+++ tests/restricted-numbers-tests.cpp
@@ -0,0 +1,239 @@
+#define CATCH_CONFIG_RUNNER
+
+#include <catch2/catch.hpp>
+#include "../restricted-number.h"
+
+using namespace std;
+
+int main(int argc, char **argv) {
+    ios::sync_with_stdio(false);
+
+    int result = Catch::Session().run( argc, argv );
+
+    // global clean-up...
+
+    return ( result < 0xff ? result : 0xff );
+}
+
+
+TEST_CASE("constructor") {
+    restricted_number<int> num(0, 100, 55);
+    REQUIRE(num.minimum == 0);
+    REQUIRE(num.maximum == 100);
+    REQUIRE(num.current == 55);
+
+    restricted_number<float> numf(0, 100, 55);
+    REQUIRE(numf.minimum == 0);
+    REQUIRE(numf.maximum == 100);
+    REQUIRE(numf.current == 55);
+}
+
+TEST_CASE("constructor without current") {
+    restricted_number<int> num(0, 100);
+    REQUIRE(num.minimum == 0);
+    REQUIRE(num.maximum == 100);
+    REQUIRE(num.current == 100);
+
+    restricted_number<float> numf(0, 100);
+    REQUIRE(numf.minimum == 0);
+    REQUIRE(numf.maximum == 100);
+    REQUIRE(numf.current == 100);
+}
+
+// Catch2 doesn't support REQUIRE_ABORT yet see https://github.com/catchorg/Catch2/issues/853
+//TEST_CASE("constructor should assert when min > max") {
+//    REQUIRE_ABORT(restricted_number<int> num(100, 0, 0));
+//    REQUIRE_ABORT(restricted_number<int> num(100, 0));
+//}
+
+TEST_CASE("constructor should cap values") {
+    restricted_number<int> num(0, 100, 101);
+    REQUIRE(num.minimum == 0);
+    REQUIRE(num.maximum == 100);
+    REQUIRE(num.current == 100);
+
+    restricted_number<int> num2(0, 100, -1);
+    REQUIRE(num2.minimum == 0);
+    REQUIRE(num2.maximum == 100);
+    REQUIRE(num2.current == 0);
+}
+
+TEST_CASE("Bounds checking should work") {
+    restricted_number<int> num(0, 100, 101);
+    REQUIRE(num.at_maximum());
+
+    restricted_number<int> num2(0, 100, -1);
+    REQUIRE(num2.at_minimum());
+}
+
+TEST_CASE("Simple value comparisons should all make sense") {
+    restricted_number<int> num(0, 100, 60);
+
+    REQUIRE(num.gt(59));
+    REQUIRE(!num.gt(60));
+    REQUIRE(!num.gt(61));
+
+    REQUIRE(!num.lt(59));
+    REQUIRE(!num.lt(60));
+    REQUIRE(num.lt(61));
+
+    REQUIRE(num.gte(59));
+    REQUIRE(num.gte(60));
+    REQUIRE(!num.gte(61));
+
+    REQUIRE(!num.lte(59));
+    REQUIRE(num.lte(60));
+    REQUIRE(num.lte(61));
+
+    restricted_number<float> num2(0, 100, 60);
+
+    REQUIRE(num2.gt(59));
+    REQUIRE(!num2.gt(60));
+    REQUIRE(!num2.gt(61));
+
+    REQUIRE(!num2.lt(59));
+    REQUIRE(!num2.lt(60));
+    REQUIRE(num2.lt(61));
+
+    REQUIRE(num2.gte(59));
+    REQUIRE(num2.gte(60));
+    REQUIRE(!num2.gte(61));
+
+    REQUIRE(!num2.lte(59));
+    REQUIRE(num2.lte(60));
+    REQUIRE(num2.lte(61));
+}
+
+TEST_CASE("Percentage value comparisons should all make sense") {
+    restricted_number<int> num(0, 100, 60);
+
+    REQUIRE(num.gt_percent(59));
+    REQUIRE(!num.gt_percent(60));
+    REQUIRE(!num.gt_percent(61));
+
+    REQUIRE(!num.lt_percent(59));
+    REQUIRE(!num.lt_percent(60));
+    REQUIRE(num.lt_percent(61));
+
+    REQUIRE(num.gte_percent(59));
+    REQUIRE(num.gte_percent(60));
+    REQUIRE(!num.gte_percent(61));
+
+    REQUIRE(!num.lte_percent(59));
+    REQUIRE(num.lte_percent(60));
+    REQUIRE(num.lte_percent(61));
+
+    restricted_number<float> num2(0, 100, 60);
+
+    REQUIRE(num2.gt_percent(59));
+    REQUIRE(!num2.gt_percent(60));
+    REQUIRE(!num2.gt_percent(61));
+
+    REQUIRE(!num2.lt_percent(59));
+    REQUIRE(!num2.lt_percent(60));
+    REQUIRE(num2.lt_percent(61));
+
+    REQUIRE(num2.gte_percent(59));
+    REQUIRE(num2.gte_percent(60));
+    REQUIRE(!num2.gte_percent(61));
+
+    REQUIRE(!num2.lte_percent(59));
+    REQUIRE(num2.lte_percent(60));
+    REQUIRE(num2.lte_percent(61));
+}
+
+TEST_CASE("Simple value mutators should all work correctly") {
+    restricted_number<int> RN1(0, 100, 50);
+
+    REQUIRE(RN1.add(10)->current == 60);
+    REQUIRE(RN1.add(50)->current == 100);
+
+    restricted_number<int> RN2(0, 100, 50);
+
+    REQUIRE(RN2.sub(10)->current == 40);
+    REQUIRE(RN2.sub(50)->current == 0);
+
+    restricted_number<int> RN3(0, 100, 50);
+
+    REQUIRE(RN3.to_minimum()->current == 0);
+
+    restricted_number<int> RN4(0, 100, 50);
+
+    REQUIRE(RN4.to_maximum()->current == 100);
+}
+
+TEST_CASE("Simple float value mutators should all work correctly") {
+    restricted_number<float> RN1(0, 100, 50);
+
+    REQUIRE(RN1.add(10)->current == 60);
+    REQUIRE(RN1.add(50)->current == 100);
+
+    restricted_number<float> RN2(0, 100, 50);
+
+    REQUIRE(RN2.sub(10)->current == 40);
+    REQUIRE(RN2.sub(50)->current == 0);
+
+    restricted_number<float> RN3(0, 100, 50);
+
+    REQUIRE(RN3.to_minimum()->current == 0);
+
+    restricted_number<float> RN4(0, 100, 50);
+
+    REQUIRE(RN4.to_maximum()->current == 100);
+}
+
+TEST_CASE("Percentage value mutators should all work correctly") {
+    restricted_number<int> RN1(0, 100, 50);
+
+    REQUIRE(RN1.add_percent(10)->current == 60);
+    REQUIRE(RN1.add_percent(50)->current == 100);
+
+    restricted_number<int> RN2(0, 100, 50);
+
+    REQUIRE(RN2.sub_percent(10)->current == 40);
+    REQUIRE(RN2.sub_percent(50)->current == 0);
+
+    restricted_number<int> RN3(0, 100, 50);
+
+    REQUIRE(RN3.set_to_percent(10)->current == 10);
+}
+
+TEST_CASE("Percentage float value mutators should all work correctly") {
+    restricted_number<float> RN1(0, 100, 50);
+
+    REQUIRE(RN1.add_percent(10)->current == 60);
+    REQUIRE(RN1.add_percent(50)->current == 100);
+
+    restricted_number<float> RN2(0, 100, 50);
+
+    REQUIRE(RN2.sub_percent(10)->current == 40);
+    REQUIRE(RN2.sub_percent(50)->current == 0);
+
+    restricted_number<float> RN3(0, 100, 50);
+
+    REQUIRE(RN3.set_to_percent(10)->current == 10);
+}
+
+TEST_CASE("Complex value mutators should all work correctly") {
+    restricted_number<int> RN1(0, 100, 50);
+
+    REQUIRE(RN1.add_over_maximum(10)->current == 60);
+    REQUIRE(RN1.maximum == 110);
+
+    restricted_number<int> RN2(0, 100, 50);
+
+    REQUIRE(RN2.sub_under_minimum(10)->current == 40);
+    REQUIRE(RN2.minimum == -10);
+}
+
+TEST_CASE("Complex float value mutators should all work correctly") {
+    restricted_number<float> RN1(0, 100, 50);
+
+    REQUIRE(RN1.add_over_maximum(10)->current == 60);
+    REQUIRE(RN1.maximum == 110);
+
+    restricted_number<float> RN2(0, 100, 50);
+
+    REQUIRE(RN2.sub_under_minimum(10)->current == 40);
+    REQUIRE(RN2.minimum == -10);
+}
