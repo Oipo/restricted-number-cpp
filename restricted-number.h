f84cfee84b161d8724de1af66a1ee0514ce04712
Michael de Lang <kingoipo@gmail.com>
2023-11-19 15:59:31 +0100

    Add const correctness, fix some warnings and add equals operator overloading

--- restricted-number.h
+++ restricted-number.h
@@ -8,115 +8,140 @@
 
 template <class T>
 struct restricted_number final {
-    T minimum, maximum, current;
-
     restricted_number(T minimum, T maximum, T current) noexcept {
         assert(minimum <= maximum);
 
-        this->minimum = minimum;
-        this->maximum = maximum;
+        _minimum = minimum;
+        _maximum = maximum;
         set(current);
     }
-
 
     restricted_number(T minimum, T maximum) noexcept {
         assert(minimum <= maximum);
 
-        this->minimum = minimum;
-        this->maximum = maximum;
-        this->current = maximum;
+        _minimum = minimum;
+        _maximum = maximum;
+        _current = maximum;
     }
 
-    restricted_number* set(T num) noexcept {
-        num = std::min(num, maximum);
-        num = std::max(num, minimum);
-        current = num;
-        return this;
+    restricted_number& set(T num) noexcept {
+        num = std::min(num, _maximum);
+        num = std::max(num, _minimum);
+        _current = num;
+        return *this;
     }
 
-    restricted_number* add(T num) noexcept {
-        return set(current + num);
+    restricted_number& set_maximum(T maximum) noexcept {
+        assert(_minimum <= maximum);
+
+        _maximum = maximum;
+        _current = std::min(_current, maximum);
+        return *this;
     }
 
-    restricted_number* sub(T num) noexcept {
+    restricted_number& set_minimum(T minimum) noexcept {
+        assert(minimum <= _maximum);
+
+        _minimum = minimum;
+        _current = std::max(_current, _minimum);
+        return *this;
+    }
+
+    restricted_number& add(T num) noexcept {
+        return set(_current + num);
+    }
+
+    restricted_number& sub(T num) noexcept {
         return add(-num);
     }
 
-    restricted_number* add_over_maximum(T num) noexcept {
-        maximum += num;
+    restricted_number& add_over_maximum(T num) noexcept {
+        _maximum += num;
         return add(num);
     }
 
-    restricted_number* sub_under_minimum(T num) noexcept {
-        minimum -= num;
+    restricted_number& sub_under_minimum(T num) noexcept {
+        _minimum -= num;
         return sub(num);
     }
 
-    restricted_number* to_maximum() noexcept {
-        return set(maximum);
+    restricted_number& to_maximum() noexcept {
+        return set(_maximum);
     }
 
-    restricted_number* to_minimum() noexcept {
-        return set(minimum);
+    restricted_number& to_minimum() noexcept {
+        return set(_minimum);
     }
 
-    bool at_maximum() noexcept {
-        return current == maximum;
+    bool at_maximum() const noexcept {
+        return _current == _maximum;
     }
 
-    bool at_minimum() noexcept {
-        return current == minimum;
+    bool at_minimum() const noexcept {
+        return _current == _minimum;
     }
 
-    bool gt(T num) noexcept {
-        return current > num;
+    bool gt(T num) const noexcept {
+        return _current > num;
     }
 
-    bool gte(T num) noexcept {
-        return current >= num;
+    bool gte(T num) const noexcept {
+        return _current >= num;
     }
 
-    bool lt(T num) noexcept {
-        return current < num;
+    bool lt(T num) const noexcept {
+        return _current < num;
     }
 
-    bool lte(T num) noexcept {
-        return current <= num;
+    bool lte(T num) const noexcept {
+        return _current <= num;
     }
 
-    restricted_number* set_to_percent(T perc) noexcept {
-        return set(perc * ((this->maximum - this->minimum) / 100));
+    restricted_number& set_to_percent(T perc) noexcept {
+        return set(perc * ((_maximum - _minimum) / 100));
     }
 
-    restricted_number* add_percent(T perc) noexcept {
-        return add(perc * ((this->maximum - this->minimum) / 100));
+    restricted_number& add_percent(T perc) noexcept {
+        return add(perc * ((_maximum - _minimum) / 100));
     }
 
-    restricted_number* sub_percent(T perc) noexcept {
+    restricted_number& sub_percent(T perc) noexcept {
         return add_percent(-perc);
     }
 
-    T as_percent() noexcept {
+    T as_percent() const noexcept {
         if(std::numeric_limits<T>::is_integer) {
-            return std::floor((static_cast<float>(current) / static_cast<float>(maximum)) * 100.f);
+            return std::floor((static_cast<float>(_current) / static_cast<float>(_maximum)) * 100.f);
         }
-        return std::floor((current / maximum) * static_cast<T>(100));
+        return std::floor((_current / _maximum) * static_cast<T>(100));
     }
 
-    bool gt_percent(T perc) noexcept {
+    T minimum() const noexcept {
+        return _minimum;
+    }
+
+    T maximum() const noexcept {
+        return _maximum;
+    }
+
+    T current() const noexcept {
+        return _current;
+    }
+
+    bool gt_percent(T perc) const noexcept {
         auto p = as_percent();
         return as_percent() > perc;
     }
 
-    bool gte_percent(T perc) noexcept {
+    bool gte_percent(T perc) const noexcept {
         return as_percent() >= perc;
     }
 
-    bool lt_percent(T perc) noexcept {
+    bool lt_percent(T perc) const noexcept {
         return as_percent() < perc;
     }
 
-    bool lte_percent(T perc) noexcept {
+    bool lte_percent(T perc) const noexcept {
         return as_percent() <= perc;
     }
 
@@ -131,26 +156,30 @@ struct restricted_number final {
     }
 
     void operator/=(const T& other) noexcept {
-        set(current/other);
+        set(_current/other);
     }
 
     void operator*=(const T& other) noexcept {
-        set(current*other);
+        set(_current*other);
     }
 
-    bool operator>(const T& other) noexcept {
+    bool operator>(const T& other) const noexcept {
         return gt(other);
     }
 
-    bool operator>=(const T& other) noexcept {
+    bool operator>=(const T& other) const noexcept {
         return gte(other);
     }
 
-    bool operator<(const T& other) noexcept {
+    bool operator<(const T& other) const noexcept {
         return lt(other);
     }
 
-    bool operator<=(const T& other) noexcept {
+    bool operator==(const T& rhs) const noexcept {
+        return _current == rhs;
+    }
+
+    bool operator<=(const T& other) const noexcept {
         return lte(other);
     }
 
@@ -160,7 +189,10 @@ struct restricted_number final {
     }
 
     friend std::ostream& operator<<(std::ostream& os, const restricted_number<T>& rn) noexcept {
-        os << "(" << rn.minimum << ", " << rn.maximum << ", " << rn.current << ")";
+        os << "(" << rn.minimum() << ", " << rn.maximum() << ", " << rn.current() << ")";
         return os;
     }
+
+private:
+    T _minimum, _maximum, _current;
 };
--- tests/restricted-numbers-tests.cpp
+++ tests/restricted-numbers-tests.cpp
@@ -5,34 +5,42 @@
 
 using namespace std;
 
+static_assert(std::is_trivially_copy_constructible<restricted_number<int>>::value, "");
+static_assert(std::is_trivially_copyable<restricted_number<int>>::value, "");
+static_assert(std::is_trivially_move_constructible<restricted_number<int>>::value, "");
+static_assert(std::is_nothrow_swappable<restricted_number<int>>::value, "");
+static_assert(std::is_nothrow_copy_constructible<restricted_number<int>>::value, "");
+static_assert(std::is_nothrow_swappable<restricted_number<int>>::value, "");
+static_assert(std::is_nothrow_move_constructible<restricted_number<int>>::value, "");
+static_assert(std::is_nothrow_copy_assignable<restricted_number<int>>::value, "");
 
 TEST_CASE("constructor") {
     restricted_number<int> num(0, 100, 55);
-    REQUIRE(num.minimum == 0);
-    REQUIRE(num.maximum == 100);
-    REQUIRE(num.current == 55);
+    REQUIRE(num.minimum() == 0);
+    REQUIRE(num.maximum() == 100);
+    REQUIRE(num == 55);
 
     restricted_number<float> numf(0, 100, 55);
-    REQUIRE(numf.minimum == 0);
-    REQUIRE(numf.maximum == 100);
-    REQUIRE(numf.current == 55);
+    REQUIRE(numf.minimum() == 0);
+    REQUIRE(numf.maximum() == 100);
+    REQUIRE(numf == 55);
 
     restricted_number<int> num2(0, 0, 0);
-    REQUIRE(num2.minimum == 0);
-    REQUIRE(num2.maximum == 0);
-    REQUIRE(num2.current == 0);
+    REQUIRE(num2.minimum() == 0);
+    REQUIRE(num2.maximum() == 0);
+    REQUIRE(num2 == 0);
 }
 
 TEST_CASE("constructor without current") {
     restricted_number<int> num(0, 100);
-    REQUIRE(num.minimum == 0);
-    REQUIRE(num.maximum == 100);
-    REQUIRE(num.current == 100);
+    REQUIRE(num.minimum() == 0);
+    REQUIRE(num.maximum() == 100);
+    REQUIRE(num == 100);
 
     restricted_number<float> numf(0, 100);
-    REQUIRE(numf.minimum == 0);
-    REQUIRE(numf.maximum == 100);
-    REQUIRE(numf.current == 100);
+    REQUIRE(numf.minimum() == 0);
+    REQUIRE(numf.maximum() == 100);
+    REQUIRE(numf == 100);
 }
 
 // Catch2 doesn't support REQUIRE_ABORT yet see https://github.com/catchorg/Catch2/issues/853
@@ -43,14 +51,14 @@ TEST_CASE("constructor without current") {
 
 TEST_CASE("constructor should cap values") {
     restricted_number<int> num(0, 100, 101);
-    REQUIRE(num.minimum == 0);
-    REQUIRE(num.maximum == 100);
-    REQUIRE(num.current == 100);
+    REQUIRE(num.minimum() == 0);
+    REQUIRE(num.maximum() == 100);
+    REQUIRE(num == 100);
 
     restricted_number<int> num2(0, 100, -1);
-    REQUIRE(num2.minimum == 0);
-    REQUIRE(num2.maximum == 100);
-    REQUIRE(num2.current == 0);
+    REQUIRE(num2.minimum() == 0);
+    REQUIRE(num2.maximum() == 100);
+    REQUIRE(num2 == 0);
 }
 
 TEST_CASE("Bounds checking should work") {
@@ -67,18 +75,30 @@ TEST_CASE("Simple value comparisons should all make sense") {
     REQUIRE(num.gt(59));
     REQUIRE(!num.gt(60));
     REQUIRE(!num.gt(61));
+    REQUIRE(num >59);
+    REQUIRE(!(num > 60));
+    REQUIRE(!(num > 61));
 
     REQUIRE(!num.lt(59));
     REQUIRE(!num.lt(60));
     REQUIRE(num.lt(61));
+    REQUIRE(!(num < 59));
+    REQUIRE(!(num < 60));
+    REQUIRE(num < 61);
 
     REQUIRE(num.gte(59));
     REQUIRE(num.gte(60));
     REQUIRE(!num.gte(61));
+    REQUIRE(num >= 59);
+    REQUIRE(num >= 60);
+    REQUIRE(!(num >= 61));
 
     REQUIRE(!num.lte(59));
     REQUIRE(num.lte(60));
     REQUIRE(num.lte(61));
+    REQUIRE(!(num <= 59));
+    REQUIRE(num <= 60);
+    REQUIRE(num <= 61);
 
     restricted_number<float> num2(0, 100, 60);
 
@@ -140,97 +160,98 @@ TEST_CASE("Percentage value comparisons should all make sense") {
 TEST_CASE("Simple value mutators should all work correctly") {
     restricted_number<int> RN1(0, 100, 50);
 
-    REQUIRE(RN1.add(10)->current == 60);
-    REQUIRE(RN1.add(50)->current == 100);
+    REQUIRE(RN1.add(10) == 60);
+    REQUIRE(RN1.add(50) == 100);
+    REQUIRE(RN1 == 100.0f);
 
     restricted_number<int> RN2(0, 100, 50);
 
-    REQUIRE(RN2.sub(10)->current == 40);
-    REQUIRE(RN2.sub(50)->current == 0);
+    REQUIRE(RN2.sub(10) == 40);
+    REQUIRE(RN2.sub(50) == 0);
 
     restricted_number<int> RN3(0, 100, 50);
 
-    REQUIRE(RN3.to_minimum()->current == 0);
+    REQUIRE(RN3.to_minimum() == 0);
 
     restricted_number<int> RN4(0, 100, 50);
 
-    REQUIRE(RN4.to_maximum()->current == 100);
+    REQUIRE(RN4.to_maximum() == 100);
 }
 
 TEST_CASE("Simple float value mutators should all work correctly") {
     restricted_number<float> RN1(0, 100, 50);
 
-    REQUIRE(RN1.add(10)->current == 60);
-    REQUIRE(RN1.add(50)->current == 100);
+    REQUIRE(RN1.add(10) == 60);
+    REQUIRE(RN1.add(50) == 100);
 
     restricted_number<float> RN2(0, 100, 50);
 
-    REQUIRE(RN2.sub(10)->current == 40);
-    REQUIRE(RN2.sub(50)->current == 0);
+    REQUIRE(RN2.sub(10) == 40);
+    REQUIRE(RN2.sub(50) == 0);
 
     restricted_number<float> RN3(0, 100, 50);
 
-    REQUIRE(RN3.to_minimum()->current == 0);
+    REQUIRE(RN3.to_minimum() == 0);
 
     restricted_number<float> RN4(0, 100, 50);
 
-    REQUIRE(RN4.to_maximum()->current == 100);
+    REQUIRE(RN4.to_maximum() == 100);
 }
 
 TEST_CASE("Percentage value mutators should all work correctly") {
     restricted_number<int> RN1(0, 100, 50);
 
-    REQUIRE(RN1.add_percent(10)->current == 60);
-    REQUIRE(RN1.add_percent(50)->current == 100);
+    REQUIRE(RN1.add_percent(10) == 60);
+    REQUIRE(RN1.add_percent(50) == 100);
 
     restricted_number<int> RN2(0, 100, 50);
 
-    REQUIRE(RN2.sub_percent(10)->current == 40);
-    REQUIRE(RN2.sub_percent(50)->current == 0);
+    REQUIRE(RN2.sub_percent(10) == 40);
+    REQUIRE(RN2.sub_percent(50) == 0);
 
     restricted_number<int> RN3(0, 100, 50);
 
-    REQUIRE(RN3.set_to_percent(10)->current == 10);
+    REQUIRE(RN3.set_to_percent(10) == 10);
 }
 
 TEST_CASE("Percentage float value mutators should all work correctly") {
     restricted_number<float> RN1(0, 100, 50);
 
-    REQUIRE(RN1.add_percent(10)->current == 60);
-    REQUIRE(RN1.add_percent(50)->current == 100);
+    REQUIRE(RN1.add_percent(10) == 60);
+    REQUIRE(RN1.add_percent(50) == 100);
 
     restricted_number<float> RN2(0, 100, 50);
 
-    REQUIRE(RN2.sub_percent(10)->current == 40);
-    REQUIRE(RN2.sub_percent(50)->current == 0);
+    REQUIRE(RN2.sub_percent(10) == 40);
+    REQUIRE(RN2.sub_percent(50) == 0);
 
     restricted_number<float> RN3(0, 100, 50);
 
-    REQUIRE(RN3.set_to_percent(10)->current == 10);
+    REQUIRE(RN3.set_to_percent(10) == 10);
 }
 
 TEST_CASE("Complex value mutators should all work correctly") {
     restricted_number<int> RN1(0, 100, 50);
 
-    REQUIRE(RN1.add_over_maximum(10)->current == 60);
-    REQUIRE(RN1.maximum == 110);
+    REQUIRE(RN1.add_over_maximum(10) == 60);
+    REQUIRE(RN1.maximum() == 110);
 
     restricted_number<int> RN2(0, 100, 50);
 
-    REQUIRE(RN2.sub_under_minimum(10)->current == 40);
-    REQUIRE(RN2.minimum == -10);
+    REQUIRE(RN2.sub_under_minimum(10) == 40);
+    REQUIRE(RN2.minimum() == -10);
 }
 
 TEST_CASE("Complex float value mutators should all work correctly") {
     restricted_number<float> RN1(0, 100, 50);
 
-    REQUIRE(RN1.add_over_maximum(10)->current == 60);
-    REQUIRE(RN1.maximum == 110);
+    REQUIRE(RN1.add_over_maximum(10) == 60);
+    REQUIRE(RN1.maximum() == 110);
 
     restricted_number<float> RN2(0, 100, 50);
 
-    REQUIRE(RN2.sub_under_minimum(10)->current == 40);
-    REQUIRE(RN2.minimum == -10);
+    REQUIRE(RN2.sub_under_minimum(10) == 40);
+    REQUIRE(RN2.minimum() == -10);
 }
 
 TEST_CASE("+= operator overloads work") {
@@ -238,21 +259,21 @@ TEST_CASE("+= operator overloads work") {
 
     num += 10;
 
-    REQUIRE(num.current == 60);
+    REQUIRE(num == 60);
 
     num += 100;
 
-    REQUIRE(num.current == 100);
+    REQUIRE(num == 100);
 
     restricted_number<float> numf(0, 100, 50);
 
     numf += 10.5;
 
-    REQUIRE(numf.current == 60.5f);
+    REQUIRE(numf == 60.5f);
 
     numf += 100;
 
-    REQUIRE(numf.current == 100);
+    REQUIRE(numf == 100);
 }
 
 TEST_CASE("-= operator overloads work") {
@@ -260,21 +281,21 @@ TEST_CASE("-= operator overloads work") {
 
     num -= 10;
 
-    REQUIRE(num.current == 40);
+    REQUIRE(num == 40);
 
     num -= 100;
 
-    REQUIRE(num.current == 0);
+    REQUIRE(num == 0);
 
     restricted_number<float> numf(0, 100, 50);
 
     numf -= 10.5;
 
-    REQUIRE(numf.current == 39.5f);
+    REQUIRE(numf == 39.5f);
 
     numf -= 100;
 
-    REQUIRE(numf.current == 0);
+    REQUIRE(numf == 0);
 }
 
 TEST_CASE("/= operator overloads work") {
@@ -282,13 +303,13 @@ TEST_CASE("/= operator overloads work") {
 
     num /= 10;
 
-    REQUIRE(num.current == 5);
+    REQUIRE(num == 5);
 
     restricted_number<float> numf(0, 100, 50);
 
     numf /= 10;
 
-    REQUIRE(numf.current == 5);
+    REQUIRE(numf == 5);
 }
 
 TEST_CASE("*= operator overloads work") {
@@ -296,19 +317,19 @@ TEST_CASE("*= operator overloads work") {
 
     num *= 2;
 
-    REQUIRE(num.current == 40);
+    REQUIRE(num == 40);
 
     num *= 10;
 
-    REQUIRE(num.current == 100);
+    REQUIRE(num == 100);
 
     restricted_number<float> numf(0, 100, 20.5);
 
     numf *= 2;
 
-    REQUIRE(numf.current == 41.f);
+    REQUIRE(numf == 41.f);
 
     numf *= 10;
 
-    REQUIRE(numf.current == 100);
+    REQUIRE(numf == 100);
 }
